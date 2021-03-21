69f41cba7a48a1e510f905daaa6715945976ba37
Yuliia Kushnyrenko <kushnyrenkouliia8gmail.com>
2021-03-21 20:17:27 +0200

    Add new files

---
+++ script.js
@@ -0,0 +1,20 @@
+//const shouldRenew = confirm('Хотите подписаться?');
+//console.log(shouldRenew);
+
+//let userInput = prompt('Введите число 5');
+//userInput = Number(userInput);
+//console.log(typeof userInput);
+
+//const value = 'cat';
+//const number = Number(value);
+//console.log(number);
+
+//const blockWidth = '300.25px';
+//const width = Number.parseFloat(blockWidth);
+//console.log(width);
+
+//let number = prompt('Введите число');
+//number = Number(number);
+
+const result = Math.round(1.9);
+console.log(result);
