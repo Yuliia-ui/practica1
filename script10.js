832317e7cbbe360f3c6eeae66b9fa6888975159b
Yuliia Kushnyrenko <kushnyrenkouliia8gmail.com>
2021-04-10 22:10:15 +0300

    Add changed

---
+++ script10.js
@@ -0,0 +1,25 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function(arr, prop) {
+  const AllPropValues = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (prop in arr[i]) {
+      AllPropValues.push(arr[i][prop]);
+    }
+  }
+  return AllPropValues;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
